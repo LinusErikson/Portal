7d70cfe36bbf0c3dccbb6fed123c0bef9d57895f
Linus Erikson <linuserikson@gmail.com>
2017-05-16 12:55:07 +0200

    Create js16.js

---
+++ script/js16.js
@@ -0,0 +1,41 @@
+class Countries extends React.Component {
+  constructor() {
+    super();
+    this.state = { countries: [] };
+    
+  }
+
+  componentDidMount() {  
+  fetch('https://forverkliga.se/JavaScript/api/simple.php?world=all')
+      .then(response => response.json())
+      .then(json => this.setState({ countries: json }));debugger;
+  }
+  
+  
+  delete(item){
+    const newState = this.state.countries;
+    if (newState.indexOf(item) > -1) {
+      newState.splice(newState.indexOf(item), 1);
+      this.setState({data: newState})
+    }
+  }
+  
+
+  render() {debugger;
+    return (
+      <div>
+        <h1>Countries</h1>
+        {
+          this.state.countries.length == 0 ? 'Loading countries..' : this.state.countries.map(country => (
+             <ul>
+              {country.name} <button onClick={this.delete.bind(this, country)}>delete</button>
+             </ul>
+            ))
+        }
+         <label>{this.state.countries.length}</label>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Countries />, document.getElementById('container'));
